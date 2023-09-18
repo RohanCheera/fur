d50806f618888093af86f80dfdefd721a4bd0287
RohanCheera <cheerarohan@gmail.com>
2023-09-18 20:03:10 +0530

    folders updation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import {Route,Routes} from 'react-router-dom';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Layout from './pages/layout/Layout';
+import Contact from './pages/contact/Contact';
+import Notifications from './pages/notifications/Notifications';
+import Profile from './pages/profile/Profile'
 
 function App() {
   return (
     <div className="App">
      <Routes>
-      <Route/>
+      <Route path='/' element={<Layout/>}>
+      <Route index element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/notifications' element={<Notifications/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      </Route>
      </Routes>
     </div>
   );
---
+++ src/pages/layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './layout.css';
+import { Outlet } from 'react-router-dom';
+
+function Layout() {
+  return (
+    <div>
+        hellooooooo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        <Outlet/>
+    </div>
+  )
+}
+
+export default Layout
