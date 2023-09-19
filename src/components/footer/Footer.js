be6e67f17274c866ef67d9f9a00b0893dbe87280
RohanCheera <cheerarohan@gmail.com>
2023-09-19 23:13:16 +0530

    footer content

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,38 @@ import './footer.css'
 
 function Footer() {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+    <div className='footer-content'>
+    <div className='footer-sub1'>
+      <div className='footer-headding'>
+      Stay in touch with us
+      </div>
+      <div className='footer-headding-content'>
+      Your interest and support is invaluable to us. Sign-up to our mailing list for latest updates.
+      </div>
+    </div>
+    <div className='footer-sub2'>
+    <div className='footer-headding'>
+    Keep up to date
+      </div>
+      <div className='footer-headding-content'>
+      Sign up to our mail list.
+      </div>
+    </div>
+    <div className='footer-sub3'>
+    <div className='footer-headding'>
+    Get connected
+      </div>
+      <div className='footer-headding-content'>
+      <div className='sm-icon1'>Fb</div>
+      <div className='sm-icon1'>TW</div>
+      <div className='sm-icon1'>YT</div>
+      <div className='sm-icon1'>IG</div>
+      <div className='sm-icon1'>Ln</div>
+      </div>
+    </div>
+    </div>
+    </div>
   )
 }
 
