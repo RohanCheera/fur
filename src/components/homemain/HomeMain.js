03d6faf58b92ecc664b217e63409bb26599d822f
RohanCheera <cheerarohan@gmail.com>
2023-09-20 19:43:26 +0530

    Articles

--- src/components/homemain/HomeMain.js
+++ src/components/homemain/HomeMain.js
@@ -9,7 +9,43 @@ function HomeMain() {
           Hi,This is Rohan.
         </div>
       </div>
-      <hr className='hrhm'/>
+      <div className='home-main-marque'>
+        <div className='marque'>
+          Hi,This is Rohan.
+        </div>
+      </div>
+      <div className='home-main-article'>
+        <div className='article'>
+        <div className='article-heading'>
+        Animal Welfare Board of India
+        </div>
+        <div className='article-body'>
+        The Animal Welfare Board of India is a statutory advisory body on Animal Welfare Laws and promotes animal welfare in the country. Established in 1962 under Section 4 of the Prevention of Cruelty to Animals Act, 1960 (No. 59 of 1960), the Animal Welfare Board of India was started under the stewardship of Late Smt. Rukmini Devi Arundale, well known humanitarian. From ensuring that animal welfare laws in the country are diligently followed, to provide grants to Animal Welfare Organizations and advising the Government of India on animal welfare issues, the Board has been the face of the animal welfare movement in the country for the last 60 years. Through its services, Board ensures that animal welfare laws in the country are diligently followed, provides grants to Animal Welfare Organizations and advises the Government of India on animal welfare issues. The Board consists of 28 Members including 6 Members of Parliament (2 Members of Parliament from Rajya Sabha and 4 Members of Parliament from Lok Sabha).
+
+The Board consists of 28 Members. The term of office of Members is for a period of 3 years.
+        </div>
+        <div className='article-button'>
+          <button>Read More</button>
+        </div>
+        </div>
+        <div className='authors'>
+          <div className='author'>
+          <div className='author-photo'>
+
+          </div>
+          <div className='author-details'></div>
+          </div>
+          <div className='author'>
+          <div className='author-photo'>
+
+          </div>
+          <div className='author-details'></div>
+          </div>
+          <div className='contribute-article'>
+          Contribute  an Article
+          </div>
+        </div>
+      </div>
       
     </div>
   )
