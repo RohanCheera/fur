c99285a879eb463024e81a9c3ad9cf38df948d9b
RohanCheera <cheerarohan@gmail.com>
2023-09-21 21:40:01 +0530

    blogs page  layout external

---
+++ src/components/blogcard/BlogCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './blogcard.css'
+
+function BlogCard() {
+  return (
+    <div className='blogcard'>
+        I Am a blog card!
+    </div>
+  )
+}
+
+export default BlogCard
--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import './blog.css'
+import BlogCard from '../../components/blogcard/BlogCard'
 
 function Blog() {
+  const blogcards=[];
+  for(var i=0;i<10;i++){
+    blogcards.push(<BlogCard key={i}/>)
+  }
   return (
-    <div>Blog</div>
+    <div className='blog'>
+    <div className='blog-main'>
+      <div className='blogtitle'>Blogs....</div>
+      {blogcards}
+      <div className='blogpagenation'> for pagination</div>
+    </div>
+    <div className='blog-contents'>
+      <div className='blog-contents-title'></div>
+    </div>
+    </div>
   )
 }
 
